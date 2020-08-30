aace4d6c6a8907a349d362e90458c3f43a54b188
Daniel Bemsen <danireptor7@gmail.com>
2020-08-30 18:27:06 +0100

    implemented the navbar and hero components

--- src/Components/Header-Footer/Header-Footer.js
+++ src/Components/Header-Footer/Header-Footer.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./Header-Footer.css";
+import Logo from "./Repify.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="Footer">
-      <h2>Header</h2>
+    <div className="Header">
+      <nav className="nav navBar">
+        <Link to="/">
+          <img src={Logo} alt="Replify" />
+        </Link>
+        <ul className="navItems">
+          <li className="navLink">
+            <Link className="links" to="/login">
+              Login
+            </Link>
+            <Link className="links" to="/register">
+              Sign
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
 
 const Footer = () => {
-    return (
-      <div className="Header">
-        <h2>Header</h2>
-      </div>
-    );
-  };
+  return (
+    <div className="Footer">
+      <h2>Footer</h2>
+    </div>
+  );
+};
 
 export { Header, Footer };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Home.css";
+import { Header } from "../../Components/Header-Footer/Header-Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="Home">
-      <h2>Home Page</h2>
+      <div className="overLay">
+        <Header />
+        <div className="Hero">
+          <div className="HeroContent">
+            <h1 className="HeroHeading">Automate your payments</h1>
+            <p className="heroDes">
+              repify automate sending money to people you usually send money to
+              on a regular basis. Lets saves you the stress of manually sending
+              them money.
+            </p>
+            <a className="btn">
+              <Link className="btnLink" to="/register">Lets take care of It</Link>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
