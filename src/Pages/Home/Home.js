f7119993bd6e24feefe5e3999b00833015b58a3c
Daniel Bemsen <danireptor7@gmail.com>
2020-08-31 23:22:50 +0100

    completed hero page and rearrange files

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,13 @@
 import React from "react";
 import "./Home.css";
-import Header from "../../Components/Header/Header";
-import { Link } from "react-router-dom";
-import Footer from "../../Components/Footer/Footer";
+import Footer from "./Footer/Footer";
+import Hero from "./Hero/Hero";
 
 const Home = () => {
   return (
     <div className="Home">
-      <div className="overLay">
-        <Header />
-        <div className="Hero">
-          <div className="HeroContent">
-            <h1 className="HeroHeading">Automate your payments</h1>
-            <p className="heroDes">
-              repify automate sending money to people you usually send money to
-              on a regular basis. Lets saves you the stress of manually sending
-              them money.
-            </p>
-              <Link className="btnLink" to="/register">Lets take care of It</Link>
-          </div>
-        </div>
-      </div>
-      <Footer/>
+      <Hero />
+      <Footer />
     </div>
   );
 };
