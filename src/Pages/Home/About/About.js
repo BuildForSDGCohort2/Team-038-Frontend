247168375695a69b54674dae2c28237f7371521b
Daniel Bemsen <danireptor7@gmail.com>
2020-09-01 20:06:16 +0100

    Completed web version of About Page

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./About.css";
+import AboutGif from "../Images/Payment.gif"
+import Header from "../../../Components/Header/Header";
+
+const About = () => {
+  return (
+    <div id="About">
+      <Header />
+      <div className="AboutWrapper">
+        <div className="AboutDes">
+          <h2 className="SubHeading">About Repify</h2>
+          <h1 className="Heading PColor">payments automations</h1>
+          <p className="AboutText">
+            Imagine if you could <span className="PColor">Automate</span>  sending money to people you usually
+            send money to on a regular basis. Say your mother, father, siblings
+            or your girlfriend. This app saves you the stress of manually
+            sending them money on a set basis and not forgetting to do so so
+            everyone is happy.
+          </p>
+          <p className="AboutText Top">
+            This feature is the main catch for the app. But it can also serve as
+            an <span className="Bold">Automated Subscription</span>  service; for those of us that would like
+            to subscribe to say Spectranet or data on a regs or even DSTV or
+            Netflix.
+          </p>
+        </div>
+        <div className="AboutGif">
+            <img src={AboutGif} className="PaymentGif" alt="ReplifyGif"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
+import About from "./About/About";
 
 const Home = () => {
   return (
-    <div className="Home">
-      <Hero />
-      <Footer />
+    <div className="HomePage">
+      {/* <div className="Home"><Hero /></div> */}
+      <About />
+      {/* <Footer /> */}
     </div>
   );
 };
