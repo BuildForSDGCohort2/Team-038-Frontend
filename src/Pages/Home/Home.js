c33aeccd7b8af6d208b16d50cbb4dc976ccee631
Majeed <jidsfotech@gmail.com>
2020-09-01 09:19:55 +0100

    How we work sectin of the home page [Provides information on how the system works]

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Home.css";
 import Footer from "../../Components/Footer/Footer";
+import HowWeWork from "../../Components/How_we_work_home_section/How_we_work.js"
+
 
 const Home = () => {
   return (
     <div className="Home">
-      <h2>Home Page</h2>
-      <Footer/>
+      <HowWeWork />
+      <Footer />
     </div>
   );
 };
