3a5d9c07b4616fc9c0fe0ac36cb914ccf60f2356
Majeed <jidsfotech@gmail.com>
2020-09-16 21:43:47 +0100

    Transaction page initial commit

---
+++ src/Pages/Transactions/data.js
@@ -0,0 +1,15 @@
+const data = [/**
+    { category: "sent", amount: "200.00", user: "majeed", date: new Date("2020-01-01") },
+    { category: "recieved", amount: "5000.00", user: "frances", date: new Date("2020-02-03") },
+    { category: "sent", amount: "400.00", user: "daniel", date: new Date("2020-03-14") },
+    { category: "recieved", amount: "8000.00", user: "gideon", date: new Date("2020-05-20") },
+    { category: "sent", amount: "400.00", user: "daniel", date: new Date("2020-06-25") },
+    { category: "fail", amount: "2000.00", user: "gideon", date: new Date("2020-08-17") },
+    { category: "recieved", amount: "2000.00", user: "daniel", date: new Date("2020-04-15") }, 
+    { category: "sent", amount: "5000.00", user: "frances", date: new Date("2020-07-11") },
+    { category: "fail", amount: "600.00", user: "majeed", date: new Date("2020-09-01")},
+    { category: "recieved", amount: "6000.00", user: "majeed", date: new Date("2020-09-05") },*/
+]
+
+
+module.exports = data;
