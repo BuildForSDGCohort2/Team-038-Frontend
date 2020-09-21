b99bb3d6ccb88bc1b41b0841b71b86c6e2ff5c2a
Majeed <jidsfotech@gmail.com>
2020-09-21 16:39:10 +0100

    completed beneficireis page

--- src/Pages/Beneficiaries/Beneficiaries.js
+++ src/Pages/Beneficiaries/Beneficiaries.js
@@ -1,10 +1,42 @@
 import React from "react";
 import "./Beneficiaries.css";
+import RenderBeneficiariesTable from "./BeneficiriesTable";
+import data from "./data";
 
 const Beneficiaries = () => {
+
+  // connect to back end and fetch all beneciries data for a user
+  const fetchAllbeneficiaries = () => {
+    if (data.length === 0) {
+      return null;
+    } else {
+      return data;
+    }
+  }
   return (
-    <div className="Home">
-      <h2> Beneficiaries </h2>
+    <div className="Beneficiaries">
+      <div className="grid-Container">
+        <div className="pageTitle col-1">
+          <h3>Beneficiaries</h3>
+          <div>Personal</div>
+        </div>
+        <div className="addBeneficiries-btn col-2">
+          <button>Add Beneficiaries</button>
+        </div>
+        <div className="totalBeneficiaries col-3">
+          <div> Total Beneficiaries</div>
+          <div>17</div>
+        </div>
+        <div className="awaitingRequest col-4">
+          <div> Awaiting Request </div>
+          <div>3</div>
+        </div>
+        <div className="scheduledExpenses col-5">
+          <div> Scheduled Expenses </div>
+          <div>#42,000</div>
+        </div>
+        <RenderBeneficiariesTable beneficiaries={fetchAllbeneficiaries()} />
+      </div>
     </div>
   );
 };
